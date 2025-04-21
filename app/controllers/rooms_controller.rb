1b03ef29bfddb04fcdcf537a16fbc53a2ccb74bc
imai-lbz <imai.atsuhiro@laiblitz.co.jp>
2025-04-21 16:08:12 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,3 +1,19 @@
 class RoomsController < ApplicationController
-  @room = Room.new
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, user_uds: [])
+  end
 end
