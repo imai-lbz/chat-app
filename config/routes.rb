dc13d99d5ab6d365c6c11aeb5683f674fcbc5a54
imai-lbz <imai.atsuhiro@laiblitz.co.jp>
2025-04-21 14:30:31 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  # 単体ページのルーティング（about/contactなど）
   get 'messages/index'
+  # トップページ指定
   root to: "messages#index"
+  # CRUDをまとめてルーティング
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
