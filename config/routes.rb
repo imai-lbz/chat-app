86f9d16454658b042e6fbce638bb5184957b48f2
imai-lbz <imai.atsuhiro@laiblitz.co.jp>
2025-04-21 16:52:07 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # 単体ページのルーティング（about/contactなど）
   get 'messages/index'
+  get 'rooms/index'
   # トップページ指定
-  root to: "messages#index"
+  root to: "rooms#index"
   # CRUDをまとめてルーティング
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
