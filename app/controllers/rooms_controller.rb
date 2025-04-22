607888d6b2ab93ad8d8dff64aec8c4841dec9769
imai-lbz <imai.atsuhiro@laiblitz.co.jp>
2025-04-22 18:55:24 +0900

    サイドバーおよびメインチャット機能の修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,6 +17,6 @@ class RoomsController < ApplicationController
 
   private
   def room_params
-    params.require(:room).permit(:name, user_uds: [])
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
